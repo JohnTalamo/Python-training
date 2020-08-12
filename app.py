c77a10f27378d9854bf5e0548bf660ba535482c4
John Talamo <johnt7166@gmail.com>
2020-08-12 18:25:51 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+# age should be between 18 and 65
+age = 22
+if age >= 18 and age < 65:
+    print("Eligible")
+
+# Quiz
+if 10 == "10":
+    print("a")
+elif "bag" > "apple" and "bag" > "cat":
+    print("b")
+else:
+    print("c")
+##################
