e3c604df7fd37299d9942b471e30d90dc7f3534a
John Talamo <johnt7166@gmail.com>
2020-08-14 12:37:29 -0400

    Practicing strings

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-for number in range(3):
-    print("Check")
-for number in range(5):
-    print("test")
-for number in range(3):
-    print("Check", number + 1)
-for number in range(1, 4):
-    print("Check", number, number * ".")
-for number in range(1, 10, 2):
-    print("Check", number, number * 2)
-#######
-success = True
-for number in range(3):
-    print("Attempt")
-    if success:
-        print("Success")
-        break
-success = False
-for number in range(3):
-    print("Attempt")
-    if success:
-        print("success")
-        break
-else:
-    print("Attempted 3 times and failed")
+name = "John"
+print(len(name))
+len(name)
+fname = "John"
+lname = "Talamo"
+full = fname + " " + lname
+print(full)
+print(len(full))
+name = "bazinga"
+print(name[2:6])
